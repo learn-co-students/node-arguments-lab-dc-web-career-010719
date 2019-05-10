5f6b3958ba778211ba3755f10358975193173e51
James Clement <jameseclement@gmail.com>
2019-05-09 22:11:06 -0400

    implement random password generator CLI in node

--- password.js
+++ password.js
@@ -1,1 +1,14 @@
-// code solution here
+let makePass = length => {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+for (let i = 0; i < process.argv[3]; i++) {
+  console.log(makePass(process.argv[2]));
+}
